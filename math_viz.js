466098c2a2ed3e1990c5fd5a850c2afab77c5a4b
Alex Modee <alexmodee@n180-p65.eduroam.kth.se>
2025-12-02 15:47:56 +0100

    changed ChartObj to VisualObj child class

--- math_viz.js
+++ math_viz.js
@@ -114,169 +114,6 @@ class CanvasObj {
 }
 
 
-// kordinate system (axis)
-class ChartObj {
-
-    constructor ( id, parentCanvas=null, xRange = null, yRange = null){
-
-        this.canvas = parentCanvas;    // containing canvas (svg) id
-        this.id = id;               
-
-        this.xRange = xRange; // chart x value range; if null, use canvas x-Range
-        this.yRange = yRange; // chart y value range; if null, use canvas y-range
-
-        this.xAxisOfset;  // chart x (botom) axis ofset (from top)
-        this.yAxisOfset;            // chart y (left) axis ofset (from left edge)
-
-        this.canvas = null;                   // chart visual (svg) object
-        if (parentCanvas !== null){     // create svg if target div is specified
-            this.assigne_to_canvas( parentCanvas );
-        }
-
-    }
-
-    // get x & y scales based on value range and canvas size
-    get_scale(){
-
-        let xScale;
-        let yScale;
-
-        // if no value range specified, use canvas scale
-        if(this.xRange === null){   
-            this.xRange = this.canvas.xRange;
-        } 
-        if(this.yRange === null){
-            this.yRange = this.canvas.yRange;
-        }
-
-        xScale = d3.scaleLinear( this.xRange, [0, this.canvas.width] );
-        yScale = d3.scaleLinear( this.yRange, [this.canvas.height, 0]);
-
-        return {"x": xScale, "y": yScale};
-
-    }
-
-    // get svg axis based on scale
-    get_axis( scale ){
-
-        let xAxis = d3.axisBottom(scale.x); 
-        let yAxis = d3.axisLeft(scale.y); 
-
-        return {"x": xAxis, "y": yAxis};
-
-    }
-
-
-    // get axis ofset based on x and y range 
-    get_axis_ofset(){
-
-        let xStep = this.canvas.width / (this.xRange[1] - this.xRange[0]);
-        let yStep = (this.canvas.height) / (this.yRange[1] - this.yRange[0]);
-
-        let xAxisOfset;
-        let yAxisOfset;
-
-
-        if(this.yRange[0] < 0){     // if min y value is negative, move x-axis up 
-            this.xAxisOfset = this.canvas.height + yStep * this.yRange[0];
-            xAxisOfset = this.canvas.height + yStep * this.yRange[0];
-        } else{
-            this.xAxisOfset = this.canvas.height; 
-            xAxisOfset = this.canvas.height; 
-        }
-
-        if(this.xRange[0] < 0){     // if min x is negative, move y-axis to right
-            this.yAxisOfset = xStep * -this.xRange[0];
-            yAxisOfset = xStep * -this.xRange[0];
-        } else{
-            this.yAxisOfset = 0;
-            yAxisOfset = 0;
-        }
-
-        return {"x": xAxisOfset, "y": yAxisOfset};
-
-    }
-
-
-    // update chart parameters
-    update_chart(){
-
-        /* placeholder */
-
-    }
-
-
-    // assignr chart to dive container and create svg ellement
-    assigne_to_canvas( targetCanvas ){
-
-        this.canvas = targetCanvas;
-        this.canvas.add_child( this, (id, msg) => { this.svg_init() } );
-        this.svg_init();
-
-    }
-
-
-    svg_init(){
-
-        if (this.canvas.svg === null){
-            return;
-        }
-
-        let scale = this.get_scale();
-        let axis = this.get_axis( scale );
-        let AxisOfset = this.get_axis_ofset();
-
-        this.canvas.svg.append("g")
-            .attr("transform", `translate(0,${AxisOfset.x})`)
-            .attr("class", this.id)
-            .call(axis.x);
-
-        this.canvas.svg.append("g")
-            .attr("transform", `translate(${AxisOfset.y},0)`)
-            .attr("class", this.id)
-            .call(axis.y);
-
-        // Axes label
-
-        // x label
-        this.canvas.svg.append("text")
-            .attr("class", `x-label ${this.id}`)
-            .attr("text-anchor", "end")
-            .attr("x",  -15)
-            .attr("y", AxisOfset.x)
-            .text("x");
-
-        // y label
-        this.canvas.svg.append("text")  
-            .attr("class", `y-label ${this.id}`)
-            .attr("text-anchor", "end")
-            .attr("x", AxisOfset.y)
-            .attr("y", -15)
-            .html("y");
-
-    }
-
-
-    // remove from canvas (svg)
-    remove_from_canvas(){
-
-        if (this.canvas === null){
-            return
-        }
-
-        if(this.canvas !== null){   
-            this.canvas.svg.selectAll("."+this.id).remove();    // remove svg elements
-        }
-
-        this.canvas.removeChild(this);  // remove from canvas child list
-        this.canvas = null;            
-
-    }
-
-
-}
-
-
 
 class VisualObj{
 
@@ -320,7 +157,7 @@ class VisualObj{
         }
 
         if(this.canvas.svg !== null){
-            this.canvas.svg.selectAll("path."+this.id).remove();    // remove svg elements
+            this.canvas.svg.selectAll("."+this.id).remove();    // remove svg elements
         }
 
         this.canvas.removeChild(this);      // remove from canvas child list
@@ -334,6 +171,134 @@ class VisualObj{
     }
 
     
+}
+
+
+// kordinate system (axis)
+class ChartObj extends VisualObj{
+
+    constructor ( id, params={}, canvas=null){
+
+        super(id);
+
+        this.params = {
+            "xRange": null,
+            "yRange": null
+        };
+
+        this.parse_params(params);
+
+        this.xAxisOfset;  // chart x (botom) axis ofset (from top)
+        this.yAxisOfset;            // chart y (left) axis ofset (from left edge)
+
+        if (canvas !== null){     // create svg if target div is specified
+            this.assigne_to_canvas( canvas );
+        }
+
+    }
+
+    // get x & y scales based on value range and canvas size
+    get_scale(){
+
+        let xScale;
+        let yScale;
+
+        // if no value range specified, use canvas scale
+        if(this.params.xRange === null){   
+            this.params.xRange = this.canvas.xRange;
+        } 
+        if(this.params.yRange === null){
+            this.params.yRange = this.canvas.yRange;
+        }
+
+        xScale = d3.scaleLinear( this.params.xRange, [0, this.canvas.width] );
+        yScale = d3.scaleLinear( this.params.yRange, [this.canvas.height, 0]);
+
+        return {"x": xScale, "y": yScale};
+
+    }
+
+    // get svg axis based on scale
+    get_axis( scale ){
+
+        let xAxis = d3.axisBottom(scale.x); 
+        let yAxis = d3.axisLeft(scale.y); 
+
+        return {"x": xAxis, "y": yAxis};
+
+    }
+
+
+    // get axis ofset based on x and y range 
+    get_axis_ofset(){
+
+        let xStep = this.canvas.width / (this.params.xRange[1] - this.params.xRange[0]);
+        let yStep = (this.canvas.height) / (this.params.yRange[1] - this.params.yRange[0]);
+
+        let xAxisOfset;
+        let yAxisOfset;
+
+
+        if(this.params.yRange[0] < 0){     // if min y value is negative, move x-axis up 
+            this.xAxisOfset = this.canvas.height + yStep * this.params.yRange[0];
+            xAxisOfset = this.canvas.height + yStep * this.params.yRange[0];
+        } else{
+            this.xAxisOfset = this.canvas.height; 
+            xAxisOfset = this.canvas.height; 
+        }
+
+        if(this.params.xRange[0] < 0){     // if min x is negative, move y-axis to right
+            this.yAxisOfset = xStep * -this.params.xRange[0];
+            yAxisOfset = xStep * -this.params.xRange[0];
+        } else{
+            this.yAxisOfset = 0;
+            yAxisOfset = 0;
+        }
+
+        return {"x": xAxisOfset, "y": yAxisOfset};
+
+    }
+
+
+    svg_init(callback=()=>{return}){
+
+        if (this.canvas.svg === null){
+            return;
+        }
+
+        let scale = this.get_scale();
+        let axis = this.get_axis( scale );
+        let AxisOfset = this.get_axis_ofset();
+
+        this.canvas.svg.append("g")
+            .attr("transform", `translate(0,${AxisOfset.x})`)
+            .attr("class", this.id)
+            .call(axis.x);
+
+        this.canvas.svg.append("g")
+            .attr("transform", `translate(${AxisOfset.y},0)`)
+            .attr("class", this.id)
+            .call(axis.y);
+
+        // Axes label
+
+        // x label
+        this.canvas.svg.append("text")
+            .attr("class", `x-label ${this.id}`)
+            .attr("text-anchor", "end")
+            .attr("x",  -15)
+            .attr("y", AxisOfset.x)
+            .text("x");
+
+        // y label
+        this.canvas.svg.append("text")  
+            .attr("class", `y-label ${this.id}`)
+            .attr("text-anchor", "end")
+            .attr("x", AxisOfset.y)
+            .attr("y", -15)
+            .html("y");
+
+    }
 
 }
 
@@ -711,29 +676,27 @@ function get_tangent(fx, x){    // get tangent-line function
 
 function test_chart(){
 
-const margin = { top: 60, right: 60, bottom: 50, left: 60 },
-  width = 450 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+    const margin = { top: 60, right: 60, bottom: 50, left: 60 },
+    width = 450 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-chart1 = new ChartObj( width, height, margin, 4, 4, "tan-chart", "viz1", -2, -2);
-chart2 = new ChartObj( width, height, margin, 4, 4, "tan-chart2", null, -4, -4);
+    canvas = new CanvasObj(width, height, margin, "viz_canvas", [-6,6], [-6,6], "viz1");
+    chart = new ChartObj("chart1", {}, canvas);
 
-//chart1.remove_svg();
-//chart2.assigne_svg("viz1");
+    let btn = document.getElementById("testBtn");
+    btn.onclick = ()=>{
 
-}
+        if(chart.canvas === null){
+            chart.assigne_to_canvas(canvas);
+        } else{
+            console.log("remove");
+            chart.remove_from_canvas();
+        }
 
-function test_canvas(){
-
-    const margin = { top: 60, right: 60, bottom: 60, left: 60 },
-        width = 450 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
-
-    let canvas = new CanvasObj(width, height, margin, "canvas1", [-6,6], [-6,6]);
-    let chart = new ChartObj( "chart1", canvas);
-    canvas.assign_to_div("viz1");
+    }
 
 }
+
 
 
 function test_graph(){
@@ -746,14 +709,18 @@ function test_graph(){
     let fx = (x)=>{return 3*Math.sin(x)};
 
     let canvas = new CanvasObj(width, height, margin, "canvas1", [-6, 6], [-6, 6], "viz1");
-    let chart = new ChartObj("chart1", canvas);
+    chart = new ChartObj("chart1", {}, canvas);
     let graph1 = new GraphObj("graph1", fx, [-6, 6],{}, canvas);
 
     let btn = document.getElementById("testBtn");
     btn.onclick = ()=>{
 
-        graph1.update(new UpdateNode({"fx": (x)=>{return x**3/10}}));
-
+        if(graph1.canvas === null){
+            graph1.assigne_to_canvas(canvas);
+        } else{
+            console.log("remove");
+            graph1.remove_from_canvas();
+        }
     }
 
 }
@@ -767,7 +734,7 @@ function test_tangent(){
     let fx = (x)=>{return 3*Math.sin(x)};
 
     let canvas = new CanvasObj(width, height, margin, "canvas1", [-6, 6], [-6, 6], "viz1");
-    let chart = new ChartObj("chart1", canvas);
+    let chart = new ChartObj("chart1", {}, canvas);
     let graph = new GraphObj("graph1", fx, [-6, 6], {}, canvas);
     let tangent = new TnagentObj("tangent", fx,{"center":2}, canvas);
 
