d78cb8dc5c72df0d460f4e7edf2fdda92ba84406
Alex Modee <alex.modee@gmail.com>
2026-03-31 13:20:32 +0200

    Split question class into base class, with base parameters, and extentions classes, implementing specific question formats, implemented base functionality for single select (with radio buttons) and drop down select answer inputs

--- excercises/excercises.js
+++ excercises/excercises.js
@@ -19,117 +19,148 @@ class Excercise{
 
 class Question{
 
+    constructor(id, text, keys, parentDiv=null, className=""){
 
-    constructor(text, key, opts, type = "single", condition=()=>{return 0} ){
+        this.id = id;
+        this.N = Object.keys(keys).length;
+        this.key = keys;
+        this.answer = new Array(this.N).fill(null);
 
-        this.type = type;
+        this.passed = false;
+        this.R = 0;
 
-        this.condition = condition;
-
-        this.key = key;
-        this.answer = new Array(opts.lenght).fill(null);
-        this.options = opts;
-
-        this.container = document.createElement("div");
-        this.container.className = "quizContainer";
+        this.containterDiv = document.createElement("div");
+        this.containterDiv.id = this.id;
+        this.containterDiv.className = "quiz" + " " + className;
 
         this.textDiv = document.createElement("div");
-        this.textElement = document.createElement("p");
-        this.textElement.innerHTML = text;
-        this.textDiv.appendChild(this.textElement);
-
-        this.container.appendChild(this.textDiv);
+        this.text = document.createElement("p");
+        this.text.innerHTML = text;
+        this.textDiv.appendChild(this.text);
 
         this.answerDiv = document.createElement("div");
-        
-        this.container.appendChild(this.answerDiv);
 
-        this.set_options();
+        this.containterDiv.appendChild(this.textDiv);
+        this.containterDiv.appendChild(this.answerDiv);
 
-    }
+        this.selector = null;
 
-
-    set_inputs(){
-
+        this.assign_to_div(parentDiv);
 
     }
 
 
-    set_answer(i){
+    assign_to_div(div){
 
-        this.answer = this.options
-
-    }
-
-
-    set_options(){
-
-
-        while(this.answerDiv.lastChild){
-            this.answerDiv.removeChild(this.answerDiv.lastChild);
+        if(div===null){
+            return
         }
 
-        if(this.type === "single"){
-
-            this.options.forEach((opt, optI) => {
-               
-                let x = new RadioBtnObj(this.id+optI, opt, this.answerDiv);
-                x.addListener( (obj)=>{
-                    let i = obj.selectedI
-                    this.answer[optI] = opt[i];
-                } );
-
-            });
-
-        }
-
-
-        else if(this.type === "men"){
-
-            this.options.forEach((opt, optI) => {
-                console.log(opt);
-                let x = new SelectorObj(this.id+optI, opt, opt, this.answerDiv);
-                x.addListener( (obj)=>{
-                    console.log(obj);
-                    this.answer[optI] = obj;
-                } );
-
-            });
-
-        }
+        div.appendChild(this.containterDiv);
 
     }
-
+    
 
     check(){
 
-        let pass = true;
+        this.R = this.N;
 
-        this.answer.forEach( (ans, i)=>{
+        this.answer.forEach((ans, i)=>{
 
             if(ans !== this.key[i]){
-                pass = false;
+                this.R--;
             }
 
         });
 
-        console.log(pass);
+        this.passed = this.R === this.N;
+        return this.passed;
 
     }
 
+}
+
+
+class QuestionSelectOne extends Question{
+
+    constructor(id, text, keys, opts, parentDiv=null, Optheader=["a)", "b)", "c)", "d)"]){
+
+        super(id, text, keys, parentDiv );
+
+        this.options = opts;
+        this.header = Optheader;
+
+        this.set_selection();
+
+    }
+
+
+    set_selection(){
+
+        for(let i=0; i<this.N; i++){
+
+            let optL = Object.keys(this.options).length;
+            let opt = this.options[i%optL];
+            this.selector = new RadioBtnObj(this.id+"select", opt, this.answerDiv);
+
+            this.selector.addListener((obj)=>{
+
+                this.answer[i] = opt[obj.selectedI];
+
+            });
+
+        }
+
+    }
+
+}
+
+
+class QuestionMenSelect extends Question{
+
+    constructor(id, text, keys, labels, opts, parentDiv=null){
+
+        super(id, text, keys, parentDiv);
+
+        this.labels = labels;
+        this.options = opts;
+
+        this.set_selection();
+
+    }
+
+
+    set_selection(){
+
+        for(let i=0; i<this.N; i++){
+
+            let optL = Object.keys(this.options).length;
+            let opt = this.options[i%optL];
+            let labelL = Object.keys(this.labels).length;
+            let label = this.labels[i%labelL];
+            this.selector = new SelectorObj(this.id+"select", opt, opt, label, this.answerDiv);
+
+            this.selector.addListener((obj)=>{
+
+                this.answer[i] = obj.select.value;
+
+            });
+
+        }
+
+    }
 
 }
 
 
 let div = document.getElementById("content");
-
-
-let x = new Question("test question", ["ja", "na"], [["ja", "nej"], [ "yeah", "na"]], "single");
-div.appendChild(x.container);
-
-let btn = new ButtonObj("checkBtn", "check answer", div);
+//let x = new QuestionSelectOne("q1", "this is a test question", ["ja", "nej"], [["ja", "nej", "kanske"]], div);
+let x = new QuestionMenSelect("q1", "test question", ["ja", "ja", "kanske"], ["a)", "b)", "c)"],[["ja", "nej", "kanske"]], div);
+let btn = new ButtonObj("checkbtn", "check answers", div);
 btn.addListener(()=>{
 
-    x.check();
+   let p = x.check();
+   console.log(x.R);
 
-})
+
+});
